9a5deb9971bd7b1d49b431dfc226de161478c358
Howard <g8r1fl@gmail.com>
2021-03-25 14:55:51 -0400

    coded the second part of the scrape script to get the featured image url

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -8,3 +8,57 @@ def init_browser():
     executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
+def scrape():
+    browser = init_browser()
+
+    # Visit mars.nasa.gov/news
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    # get latest news title and summary paragraph
+    news_title = soup.find('div', class_='content_title').text
+    news_p = soup.find('div', class_='rollover_description_inner').text
+
+    mars_news = {
+        'news_title': news_title, 'news_p': news_p
+    }
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # open browser again for next webpage scrape
+    browser = init_browser()
+
+    # Visit mars.nasa.gov/news
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    # HTML object
+    html = browser.html
+
+    # Parse HTML with Beautiful Soup
+    soup = bs(html, 'html.parser')
+    # find the parent div of the image
+    img_url = soup.find('div', class_='floating_text_area')
+    # find the href within the div
+    ref = img_url.find('a')['href']
+    # build the final link for the featured image
+    featured_image_url = ['https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/' + ref]
+
+    # add data to dictionary
+    mars_news['featured_image_url'] = featured_image_url
+
+    # Close the browser after scraping
+    browser.quit()
+
+    
+
