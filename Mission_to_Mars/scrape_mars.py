a5a99fb3cfb3a896ccf994db5c6ecc6912b416ba
Howard <g8r1fl@gmail.com>
2021-03-25 11:45:08 -0400

    started the scrape_mars.py script

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,10 @@
+rom splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
